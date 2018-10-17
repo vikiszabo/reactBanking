55e58247c2db6f2fc664229ed5c59ecaef6580e4
Adel Kovacs <kovacsadel12@gmail.com>
2018-10-17 16:17:30 +0200

    mock db, router, dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Example from "./Example/Example";
+import Transaction from "./Transaction/Transaction";
+import {Router} from "@reach/router";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Example text="React Banking" />
-            </div>
+            <Router>
+                <Example text={"React banking"} path="/" />
+                <Transaction path="/transaction" />
+            </Router>
         );
     }
 }
