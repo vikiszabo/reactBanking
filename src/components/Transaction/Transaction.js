7e49f0f30e29a7e1047158a84c60ba5a1de77d9b
Adel Kovacs <kovacsadel12@gmail.com>
2018-10-17 19:29:03 +0200

    redux store configured

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+function mapStateToProps(state) {
+    return {transactions: state.transactions};
+}
 
 class Transaction extends Component {
+
     render() {
         return (
             <div>
-                Transaction
+                <h2>Transactions:</h2>
+                <ul>
+                    {this.props.transactions.map(txn => {
+                        return (
+                            <li key={txn.id}> {txn.id} {txn.sender} {txn.recipient} </li>
+                        )
+                    })}
+                </ul>
             </div>
         );
     }
 }
 
-export default Transaction;
+export default connect(
+    mapStateToProps,
+)(Transaction);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import configStore from "./store/configStore";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configStore();
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
