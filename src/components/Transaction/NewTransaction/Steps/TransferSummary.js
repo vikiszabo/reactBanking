fb8236704fbc7baf1dfbe1e9ea298e5b55178f50
Adel Kovacs <kovacsadel12@gmail.com>
2018-10-20 11:29:56 +0200

    transfer summary

--- src/components/Transaction/NewTransaction/Steps/TransferSummary.js
+++ src/components/Transaction/NewTransaction/Steps/TransferSummary.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Paper from "@material-ui/core/Paper/Paper";
+import Table from "@material-ui/core/Table/Table";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableCell from "@material-ui/core/TableCell/TableCell";
 
 const TransferSummary = ({transferDetails}) => {
+
+    const info = [
+        {label: "Recipient's name", value: transferDetails.recipientName},
+        {label: "Recipient's account", value: transferDetails.recipientAccount},
+        {label: "Amount to transfer", value: transferDetails.ccy + " " + transferDetails.amount},
+    ]
+
     return (
-        <div>
-            {transferDetails.amount}
-        </div>
+        <Paper>
+            <Table>
+                {info.map(info => {
+                   return (
+                            <TableRow>
+                                <TableCell>
+                                    {info.label}
+                                </TableCell>
+                                <TableCell>
+                                    {info.value}
+                                </TableCell>
+                            </TableRow>
+                    )})}
+            </Table>
+        </Paper>
     );
 };
 
-TransferSummary.propTypes = {
-
-};
+TransferSummary.propTypes = {};
 
 export default TransferSummary;
