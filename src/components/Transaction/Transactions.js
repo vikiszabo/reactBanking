d02e47724a2d0c45079d34c9df8de98c051af32d
Adel Kovacs <kovacsadel12@gmail.com>
2018-10-20 14:49:21 +0200

    transaction history table format

--- src/components/Transaction/Transactions.js
+++ src/components/Transaction/Transactions.js
@@ -1,23 +1,59 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Table from "@material-ui/core/Table/Table";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableHead from "@material-ui/core/TableHead/TableHead";
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import Paper from "@material-ui/core/Paper/Paper";
+import Typography from "@material-ui/core/Typography/Typography";
+import {withStyles} from "@material-ui/core";
 
 function mapStateToProps(state) {
     return {transactions: state.transactions};
 }
+
 
 class Transactions extends Component {
 
     render() {
         return (
             <div>
-                <h2>Transactions:</h2>
-                <ul>
-                    {this.props.transactions.map(txn => {
-                        return (
-                            <li key={txn.id}> {txn.id} {txn.sender} {txn.recipient} </li>
-                        )
-                    })}
-                </ul>
+                <Typography variant="headline">
+                    Transaction history
+                </Typography>
+                <Paper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Recipient</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>Currency</TableCell>
+                                <TableCell>Date</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.transactions.map(txn => {
+                                return (
+                                    <TableRow>
+                                        <TableCell>
+                                            {txn.recipientName}
+                                        </TableCell>
+                                        <TableCell>
+                                            {txn.amount}
+                                        </TableCell>
+                                        <TableCell>
+                                            {txn.ccy}
+                                        </TableCell>
+                                        <TableCell>
+                                            {txn.date}
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            })}
+                        </TableBody>
+                    </Table>
+                </Paper>
             </div>
         );
     }
